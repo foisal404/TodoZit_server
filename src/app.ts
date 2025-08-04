96bbd70f05b959dad0f430744e84ef155b1b7da9
foisal404 <foisalahmmed2@gmail.com>
2025-08-04 19:16:17 +0600

    basic express typescript project init

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import itemRoutes from './routes/itemRoutes';
+import { errorHandler } from './middlewares/errorHandler';
+
+const app = express();
+
+app.use(express.json());
+
+// Routes
+app.get('/', (_req, res) => {
+  res.send('Todozit API!');
+});
+
+app.use('/api/items', itemRoutes);
+
+// Global error handler (should be after routes)
+app.use(errorHandler);
+
+export default app;
