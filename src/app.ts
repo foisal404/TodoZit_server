2b5590e22ccb3808f4a32f2d61e93b98f01b497f
foisal404 <foisalahmmed2@gmail.com>
2025-08-06 13:31:36 +0600

    update routes and imports in app

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,39 @@
 import express from 'express';
-import itemRoutes from './routes/itemRoutes';
 import { errorHandler } from './middlewares/errorHandler';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.config';
+import taskRoutes from './routes/taskRoutes.routes';
+import keepRoutes from './routes/keep.routes';
+import session from 'express-session';
+import passport from 'passport';
+import './config/passport.config';
+import authRoutes from './routes/auth.routes';
+import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const app = express();
 
 app.use(express.json());
+dotenv.config();
+connectDB();
 
-// Routes
+app.use(
+  session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', (_req, res) => {
   res.send('Todozit API!');
 });
 
-app.use('/api/items', itemRoutes);
+app.use('/auth', authRoutes);
+app.use('/api/tasks', isAuthenticated, taskRoutes);
+app.use('/api/keep', isAuthenticated, keepRoutes);
 
 // Global error handler (should be after routes)
 app.use(errorHandler);
