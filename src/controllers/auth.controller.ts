61ab47091765a03f8cd3f1352fdb52e2c7060f9a
foisal404 <foisalahmmed2@gmail.com>
2025-08-06 13:30:26 +0600

    auth controller add

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+
+export const getLoggedInUser = (req: Request, res: Response) => {
+  if (req.isAuthenticated()) {
+    res.json(req.user);
+  } else {
+    res.status(401).json({ message: 'Not authenticated' });
+  }
+};
+
+// Redirect after successful login
+export const googleCallback = (req: Request, res: Response) => {
+  res.redirect('/');
+};
+
+// Logout controller
+export const logout = (req: Request, res: Response) => {
+  req.logout(() => {
+    res.redirect('/');
+  });
+};
